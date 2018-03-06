81ced4ce525d898af5114dd7a23b0380dceb6bf9
John Lamansky <lamansky@users.noreply.github.com>
2018-03-06 09:41:48 +0100

    Make `to` default to new object and add `enumOnly` and `overwrite` flags

--- test.js
+++ test.js
@@ -27,4 +27,43 @@ describe('copyOwn()', function () {
     assert.strictEqual(to.a, 1)
     assert.strictEqual(typeof to.inherited, 'undefined')
   })
+
+  it('should create a new object if no destination is specified', function () {
+    const from = {a: 1, b: 2}
+    const to = copyOwn(from)
+    assert.notStrictEqual(from, to)
+    assert.strictEqual(to.a, 1)
+    assert.strictEqual(to.b, 2)
+  })
+
+  it('should overwrite properties by default', function () {
+    const dest = {key: 1}
+    assert.strictEqual(dest.key, 1)
+    copyOwn({key: 2}, dest)
+    assert.strictEqual(dest.key, 2)
+  })
+
+  it('should silently fail to copy duplicated properties if `overwrite` is false', function () {
+    const dest = {key: 1}
+    assert.strictEqual(dest.key, 1)
+    copyOwn({key: 2}, dest, {overwrite: false})
+    assert.strictEqual(dest.key, 1)
+  })
+
+  it('should include non-enumerable properties by default', function () {
+    const source = {}
+    Object.defineProperty(source, 'noEnum', {enumerable: false, value: 123})
+    assert.strictEqual(source.noEnum, 123)
+    const dest = copyOwn(source)
+    assert.strictEqual(dest.noEnum, 123)
+  })
+
+  it('should exclude non-enumerable properties if `enumOnly` is true', function () {
+    const source = {enum: 123}
+    Object.defineProperty(source, 'noEnum', {enumerable: false, value: 234})
+    assert.strictEqual(source.noEnum, 234)
+    const dest = copyOwn(source, {}, {enumOnly: true})
+    assert.strictEqual(dest.enum, 123)
+    assert.strictEqual(typeof dest.noEnum, 'undefined')
+  })
 })
